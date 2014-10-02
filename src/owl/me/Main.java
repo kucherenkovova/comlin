703871e4b5ef05d29c01a65ea1cba3acb5fa7b0b
Vova <kucherenkovova@gmail.com>
2014-10-03 01:02:52 +0300

    Main class with nio code

--- src/owl/me/Main.java
+++ src/owl/me/Main.java
@@ -1,6 +1,6 @@
 package owl.me;
 
-import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         long total = 0;
         ArgumentParser parser = new ArgumentParser(args);
-        File directory = parser.getDirectory();
+        Path directory = parser.getDirectory();
         FileFinder finder = new FileFinder(parser.getExtensions());
-        List<File> files = finder.findFiles(directory);
-        for(File file : files) {
+        List<Path> files = finder.findFiles(directory);
+        for(Path file : files) {
             total += LineCounter.countNonEmptyLines(file);
         }
         System.out.println("There are " + total + " strings of code in your project!");
