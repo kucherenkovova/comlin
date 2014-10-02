dca934f4387f1066dc79399bafb570e13d9daf4b
Vova <kucherenkovova@gmail.com>
2014-10-03 01:01:41 +0300

    ArgumentParser with nio code

--- src/owl/me/ArgumentParser.java
+++ src/owl/me/ArgumentParser.java
@@ -1,25 +1,27 @@
 package owl.me;
 
-import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class ArgumentParser {
-    private File directory;
+    private Path directory;
     private String[] extensions;
 
     public ArgumentParser(String args[]) {
         if (args.length < 2)
             throw new IllegalArgumentException("Too short argument list");
         else {
-            directory = new File(args[0]);
-            if(!directory.isDirectory()) {
-                throw new IllegalArgumentException(directory.getName() + " is not a directory");
+            directory = Paths.get(args[0]);
+            if(!Files.isDirectory(directory)) {
+                throw new IllegalArgumentException(directory + " is not a directory");
             }
             extensions = Arrays.copyOfRange(args, 1, args.length);
         }
     }
 
-    public File getDirectory() {
+    public Path getDirectory() {
         return directory;
     }
 
